54021348fc749dc4844876b730bb3a1f3120a493
YrSdch <58011510+YrSdch@users.noreply.github.com>
2019-11-27 01:29:29 +0200

    header section is ready

--- dist/js/scripts.min.js
+++ dist/js/scripts.min.js
@@ -6,17 +6,44 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	/*===================slide menu functions=======================*/
+	
+	function slideMenu (clickElem, target, overlay){
+		$(clickElem).click(function(){
+			$(target).toggleClass('active');
+			$('body').toggleClass('stop-scroll');
+			$(overlay).toggleClass('active')
+		});
+	}
 
-	$('.hamburger').click(function(){
-		$('.slide-menu').toggleClass('active')
-		$('body').toggleClass('stop-scroll')
-		$('.overlay').toggleClass('active')
-	})
-	$('.overlay').click(function(){
-		$(this).removeClass('active')
+	function hideMenuBg (clickElem, target){ //hide menu with clicking on overlap bg
+		$(clickElem).click(function(){
+			$(this).removeClass('active')
+			$('body').removeClass('stop-scroll')
+			$(target).removeClass('active')
+		});
+	}
+
+	$('.icon-white-bag').click(function(){
+		$('.overlay-bg').removeClass('active')
+		$('.shopping-cart-wrapper').removeClass('active')
 		$('body').removeClass('stop-scroll')
-		$('.slide-menu').removeClass('active')
+
 	})
 
+	slideMenu('.hamburger', '.slide-menu', '.overlay');
+	slideMenu('.icon-shop', '.shopping-cart-wrapper', '.overlay-bg');
+	
+	hideMenuBg('.overlay-bg', '.shopping-cart-wrapper')
+	hideMenuBg('.overlay', '.slide-menu')
 
+/*===================slide menu functions=======================*/
+	$('.arrow-up-right').click(function(){
+		$(this).toggleClass('active');
+		$('.version-wrapp').toggleClass('hidden');
+		$(this).parent('.header__lower-signature').find('span').toggleClass('hidden');
+	})
+
+	
+	
+	
 });
